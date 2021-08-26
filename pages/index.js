a725eaff8b50e7c7a5b82f9694b733e204017240
David Mwendwa <klatzdavie19@gmail.com>
2021-08-26 07:30:19 +0300

    feat: add notFound page handler

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,11 @@ export async function getStaticProps() {
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
-  return { props: { products: data.products } };
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+  return { props: { products: data.products }, revalidate: 10 };
 }
 
 export default HomePage;
