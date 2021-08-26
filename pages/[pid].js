22f032ec57cddba79104090e09fbac99057863c9
David Mwendwa <klatzdavie19@gmail.com>
2021-08-26 08:30:43 +0300

    feat: configure getStaticPaths function

--- pages/[pid].js
+++ pages/[pid].js
@@ -4,6 +4,9 @@ import path from 'path';
 
 function ProductDetailPage(props) {
   const { loadedProduct } = props;
+
+  // if (!loadedProduct) return <p>Loading...</p>;
+
   return (
     <Fragment>
       <h1>{loadedProduct.title}</h1>
@@ -33,10 +36,8 @@ export async function getStaticPaths() {
   return {
     paths: [
       { params: { pid: 'p1' } },
-      { params: { pid: 'p2' } },
-      { params: { pid: 'p3' } },
     ],
-    fallback: false,
+    fallback: 'blocking',
   };
 }
 
