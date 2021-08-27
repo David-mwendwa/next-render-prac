94864b032bce4e5e835795a7af32d00b9c64d17b
David Mwendwa <klatzdavie19@gmail.com>
2021-08-27 14:30:59 +0300

    feat: add dynamic pages use _getServerSideProps_

--- pages/user-profile.js
+++ pages/user-profile.js
@@ -1,13 +1,15 @@
 function UserProfilePage(props) {
-  return <h1>{props.username}</h1>
+  return <h1>{props.username}</h1>;
 }
 
-export default UserProfilePage
+export default UserProfilePage;
 
 export async function getServerSideProps(context) {
+  const { params, req, res } = context;
+  console.log(context)
   return {
     props: {
-      username: "David"
-    }
-  }
+      username: 'David',
+    },
+  };
 }
